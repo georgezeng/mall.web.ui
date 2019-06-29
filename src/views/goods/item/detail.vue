919684167b56f3f2e673bd1cdee86139e99b0705
georgezeng <zzeng@bglcorp.com.au>
2019-06-29 17:09:15 +0800

    no message

--- src/views/goods/item/detail.vue
+++ src/views/goods/item/detail.vue
@@ -322,7 +322,7 @@
         },
         computed: {
             thumbnail() {
-                return config.publicBucketDomain + item.thumbnail
+                return config.publicBucketDomain + this.item.thumbnail
             },
             isSinglePrice() {
                 return this.item.minPrice == this.item.maxPrice
@@ -397,16 +397,15 @@
         methods: {
             share() {
                 if (Util.isInWechat()) {
-                    const self = this
                     wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
                         wx.updateAppMessageShareData({
-                            title: self.item.name, // 分享标题
-                            desc: self.item.sellingPoints, // 分享描述
+                            title: this.item.name, // 分享标题
+                            desc: this.item.sellingPoints, // 分享描述
                             link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                             imgUrl: this.thumbnail, // 分享图标
                             success: function () {
                                 // 设置成功
-                                self.$vux.toast.show({text: '请先选择数量'})
+                                this.$vux.toast.show({text: '请先选择数量'})
                             }
                         })
                     });
