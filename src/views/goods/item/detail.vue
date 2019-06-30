51965240e6c66ddf0a14ae56ac8c030b6751d9e9
georgezeng <zzeng@bglcorp.com.au>
2019-06-30 11:13:46 +0800

    no message

--- src/views/goods/item/detail.vue
+++ src/views/goods/item/detail.vue
@@ -506,13 +506,13 @@
                     const params = this.shareParams
                     soshm('#share', {
                         // 分享的链接，默认使用location.href
-                        // url: params.link,
+                        url: params.link,
                         // 分享的标题，默认使用document.title
-                        title: params.title,
+                        // title: params.title,
                         // 分享的摘要，默认使用<meta name="description" content="">content的值
-                        digest: params.desc,
+                        // digest: params.desc,
                         // 分享的图片，默认获取本页面第一个img元素的src
-                        pic: params.imgUrl,
+                        // pic: params.imgUrl,
                         // 默认显示的网站为以下六个个,支持设置的网站有
                         // weixin,weixintimeline,qq,qzone,yixin,weibo,tqq,renren,douban,tieba
                         sites: ['weixin', 'weixintimeline', 'qq', 'qzone']
