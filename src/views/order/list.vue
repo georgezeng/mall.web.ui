618f538dd6709d64b996f19513514b03db4e5736
georgezeng <zzeng@bglcorp.com.au>
2019-07-15 16:36:29 +0800

    no message

--- src/views/order/list.vue
+++ src/views/order/list.vue
@@ -165,12 +165,11 @@
                             id: item.id,
                             type: 'JSAPI'
                         }).then(data => {
-                            const self = this
                             wx.chooseWXPay({
                                 timestamp: data.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                                 nonceStr: data.nonce_str, // 支付签名随机串，不长于 32 位
                                 package: data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-                                signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                                signType: 'SHA1', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
                                 paySign: data.paySign, // 支付签名
                                 success: function (res) {
                                     // 支付成功后的回调函数
@@ -181,7 +180,7 @@
                                         //         orderNo: res.data.orderBaseinfo.orderNo
                                         //     }
                                         // })
-                                        alert(res.data.orderBaseinfo.orderNo)
+                                        alert(res.data.out_trade_no)
                                     }
                                 }
                             });
