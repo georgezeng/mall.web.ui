e06ea57ffa17b2849c4c05ab344639165f5fdf77
georgezeng <zzeng@bglcorp.com.au>
2019-07-26 21:04:08 +0800

    no message

--- src/views/order/alipay-in-wechat.vue
+++ src/views/order/alipay-in-wechat.vue
@@ -2,7 +2,7 @@
     <div v-if="isInWechat"
          style="position: fixed; background-color: rgba(0, 0, 0, 0.8); width: 100%; z-index: 100000;"
          :style="{height: minHeight}">
-        <img :src="ShareTipArrow" width="111" height="99" style="position: absolute; right: 20px;"/>
+        <img :src="TipArrow" width="111" height="99" style="position: absolute; right: 20px;"/>
         <div style="color: #fff; position: relative; top: 100px; left: 30px;">
             点击右上角的"..."，打开系统浏览器进行支付
         </div>
