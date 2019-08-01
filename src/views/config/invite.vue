ad8dd2865d1e133f8d8d388231ab81b1accdc391
georgezeng <zzeng@bglcorp.com.au>
2019-08-01 13:21:11 +0800

    no message

--- src/views/config/invite.vue
+++ src/views/config/invite.vue
@@ -239,9 +239,9 @@
                 }
                 const params = {
                     title:'注册邀请', // 分享标题
-                    desc: item.nickname + '邀请您注册成为商城会员', // 分享描述
+                    desc: (item.nickname ? item.nickname : '') + '邀请您注册成为商城会员', // 分享描述
                     link: window.location.protocol + "//" + window.location.host + "/" + uid + "#/Home", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                    imgUrl: null, // 分享图标
+                    imgUrl: this.popupImgSrc, // 分享图标
                 }
                 if (Util.isInWechat()) {
                     wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
