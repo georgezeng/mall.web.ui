9e342f4b4e6ec2eb57a01a251a26815c5364f918
georgezeng <zzeng@bglcorp.com.au>
2019-08-08 13:23:44 +0800

    no message

--- src/views/goods/search.vue
+++ src/views/goods/search.vue
@@ -114,9 +114,11 @@
             this.contentStyle.backgroundColor = '#fff'
             this.contentStyle.marginTop = '60px'
             this.contentStyle.minHeight = (document.documentElement.clientHeight - 60) + "px"
-            this.$nextTick(() => {
-                this.$refs.searchInput.$el.children[2].focus();
-            });
+            setTimeout(() => {
+                this.$nextTick(() => {
+                    this.$refs.searchInput.$el.children[2].focus();
+                })
+            }, 10)
             this.load()
         }
     }
