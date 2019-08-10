bc0c8a1a75438773ff22ec27cd94635179ea7f9d
georgezeng <zzeng@bglcorp.com.au>
2019-08-10 23:26:14 +0800

    no message

--- src/config/index.js
+++ src/config/index.js
@@ -8,19 +8,19 @@ switch (internalConfig.env) {
     case 'development':
         config.baseUrl = 'http://localhost:9002'
         config.debug = false
-        config.publicBucketDomain = 'https://malls-admin-public-local.oss-cn-shenzhen.aliyuncs.com/'
+        config.publicBucketDomain = 'https://mall-public-local.oss-cn-zhangjiakou.aliyuncs.com/'
         break
 
     case 'uat':
-        config.baseUrl = 'https://mall-server.bsxkj.com'
+        config.baseUrl = 'https://uat.mall-server.bsxkj.com'
         config.debug = false
-        config.publicBucketDomain = 'https://malls-admin-public-uat.oss-cn-shenzhen.aliyuncs.com/'
+        config.publicBucketDomain = 'https://mall-public-uat.oss-cn-zhangjiakou.aliyuncs.com/'
         break
 
     default:
-        config.baseUrl = ''
+        config.baseUrl = 'https://mall-server.bsxkj.com'
         config.debug = false
-        config.publicBucketDomain = 'https://malls-admin-public-live.oss-cn-shenzhen.aliyuncs.com/'
+        config.publicBucketDomain = 'https://mall-public-prod.oss-cn-zhangjiakou.aliyuncs.com/'
 }
 
 export default config
