0366f8dd8a043250c20cd35942de08c3a848478a
georgezeng <zzeng@bglcorp.com.au>
2019-08-14 13:11:17 +0800

    no message

--- src/views/config/invite.vue
+++ src/views/config/invite.vue
@@ -110,7 +110,7 @@
                     如果不成功请打开浏览器的菜单进行分享
                 </div>
             </div>
-            <div v-if="popup" :style="popupStyle" style="position: absolute; top: 50px;">
+            <div v-show="popup" :style="popupStyle" style="position: absolute; top: 50px;">
                 <img crossorigin="use-credentials" :src="popupImgSrc" :width="popupImgWidth" :height="popupImgHeight"/>
                 <div class="save-to-local-btn" :style="saveBtnStyle"
                      style="position: absolute; bottom: 10px; left: 10px;">
@@ -314,7 +314,7 @@
             }
         },
         mounted() {
-            this.popupImgSrc = config.baseUrl + '/client/' + Util.get('userId') + '/poster/invite.png'
+            this.popupImgSrc = config.baseUrl + '/client/' + Util.get('userId') + '/poster/invite.png?d=' + new Date().getTime()
             this.load()
             this.bgStyle = {
                 width: document.documentElement.clientWidth + 'px',
