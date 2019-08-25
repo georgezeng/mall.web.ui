00172f2a55e8685f3585c92d5a33cac3e3ac660d
georgezeng <zzeng@bglcorp.com.au>
2019-08-25 13:26:10 +0800

    no message

--- src/views/config/invite.vue
+++ src/views/config/invite.vue
@@ -246,8 +246,8 @@
                 const params = {
                     title: '注册邀请', // 分享标题
                     desc: nickname + '邀请您注册成为' + item.shopName + '会员', // 分享描述
-                    link: window.location.protocol + "//" + window.location.host + "/" + uid + "#/Home", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                    imgUrl: config.publicBucketDomain + 'client/' + item.id + '/invite/poster_' + this.md5(item) + '.png', // 分享图标
+                    link: window.location.protocol + "//" + window.location.host + "/?uid=" + uid + "#/Home", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: config.publicBucketDomain + 'common/logo.png', // 分享图标
                 }
                 if (Util.isInWechat()) {
                     wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
