59124949f754e0e29faa5e338a4d06fa749438a9
georgezeng <zzeng@bglcorp.com.au>
2019-09-03 10:42:05 +0800

    no message

--- src/views/config/center.vue
+++ src/views/config/center.vue
@@ -227,7 +227,7 @@
                         <Icon size="24" style="color: #7C502E; margin-right: 10px;" type="md-information-circle" /> 关于我们
                         <Icon type="ios-arrow-forward" size="20" class="goArrow"/>
                     </cell-box>
-                    <cell-box v-if="isSmallDevice" class="optionPanel" style="visibility: hidden; margin-top: 60px;">
+                    <cell-box class="optionPanel" style="visibility: hidden; margin-top: 60px;">
                     </cell-box>
                 </group>
             </div>
