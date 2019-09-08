2ba8e6ba7df5f9898fa6e2654d88b274b00b4ce5
georgezeng <zzeng@bglcorp.com.au>
2019-09-08 17:14:38 +0800

    no message

--- src/views/order/express.vue
+++ src/views/order/express.vue
@@ -18,8 +18,9 @@
         <Content :style="commonStyles.content" style="margin-top: 60px;">
             <div style="margin-bottom: 10px; padding: 10px 15px 0;" v-for="express in data.expressList"
                  :key="express.id">
-                <div>物流公司: {{express.company}}</div>
-                <div>物流单号: {{express.number}}</div>
+                <div>物流方式: {{express.type.text}}</div>
+                <div v-if="express.type == 'Delivery'">物流公司: {{express.company}}</div>
+                <div v-if="express.type == 'Delivery'">物流单号: {{express.number}}</div>
             </div>
         </Content>
     </Layout>
