a3c7c433c8002cde5b4839ed60faf135459a28bc
georgezeng <zzeng@bglcorp.com.au>
2019-09-11 11:12:57 +0800

    no message

--- src/views/aftersale/refund-detail.vue
+++ src/views/aftersale/refund-detail.vue
@@ -51,6 +51,7 @@
                     <div style="margin-top: 5px;">退款方式: 支付账号</div>
                     <!--<div style="margin-top: 5px;">退款金额: {{item.amount}}</div>-->
                     <div style="margin-top: 5px;">退款单号: {{item.serviceId}}</div>
+                    <div style="margin-top: 5px;">备注: {{item.remark}}</div>
                 </div>
             </div>
             <div v-if="item.rejectTime != null" style="font-size: 14px;">
